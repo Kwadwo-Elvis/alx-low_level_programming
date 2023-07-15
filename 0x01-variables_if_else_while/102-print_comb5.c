0f02921be90a0aad42c79a6265226f401210fb5d
Elvis Somuah <somuahelvis50@gmail.com>
2023-07-15 06:54:52 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,21 +7,28 @@
  */
 int main(void)
 {
-	int hundreds_digit;
-	int tens_digit;
-	int ones_digit;
+	int num1;
+	int num2;
+	int num3;
 
-	for (hundreds_digit = 0; hundreds_digit < 8; hundreds_digit++)
+	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (tens_digit = hundreds_digit + 1; tens_digit < 9; tens_digit++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (ones_digit = tens_digit + 1; ones_digit < 10; ones_digit++)
+			for (num3 = num2 + 1; num3 <= 99; num3++)
 			{
-				putchar(hundreds_digit + '0');
-				putchar(tens_digit + '0');
-				putchar(ones_digit + '0');
+				putchar((num1 / 10) + '0');
+				putchar((num1 % 10) + '0');
+				putchar(' ');
+				putchar((num2 / 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar(',');
+				putchar(' ');
 
-				if (hundreds_digit != 7 || tens_digit != 8 || ones_digit != 9)
+				putchar((num3 / 10) + '0');
+				putchar((num3 % 10) + '0');
+
+				if (num1 != 97 || num2 != 98 || num3 != 99)
 				{
 					putchar(',');
 					putchar(' ');
