7d46781563bbaa0a424706ee54303e5d7d9449bf
Elvis Somuah <somuahelvis50@gmail.com>
2023-07-18 13:13:20 -0700

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,26 +6,26 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
 	if (n <= 98)
 	{
-		for (i = n; i < 98; i++)
+		while (n < 98)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
+			n++;
 		}
 	}
 	else
 	{
-		for (i = n; i > 98; i--)
+		while (n > 98)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
 			_putchar(',');
 			_putchar(' ');
+			n--;
 		}
 	}
 
