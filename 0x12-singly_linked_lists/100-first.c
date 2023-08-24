8b9b0f7c5bdf6ba065aa3ff5a7ebc44d1bb385c9
Elvis Somuah <somuahelvis50@gmail.com>
2023-08-24 10:01:29 -0700

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-void print_before_main(void) __attribute__((constructor));
+void print_first(void) __attribute__((constructor));
 
 /**
- * print_before_main - Prints the message before the main function is executed.
+ * print_first - Prints the message before the main function is executed.
  */
-void print_before_main(void)
+void print_first(void)
 {
 	printf("You're beat! and yet, you must allow\n");
 	printf("I bore my house upon my back!\n");
