18154e068f4ff499b308c15ed32ab0891004938c
Elvis Somuah <somuahelvis50@gmail.com>
2023-08-24 09:48:21 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+void print_before_main(void) __attribute__((constructor));
+
+/**
+ * print_before_main - Prints the message before the main function is executed.
+ */
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow\n");
+	printf("I bore my house upon my back!\n");
+}
